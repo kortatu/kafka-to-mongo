93ea1a09acb78820fc0a5f4a7d72c68b58ae30c4
Alvaro <alvaro@epiclabs.io>
2019-04-08 15:22:40 +0200

    Added consul support

--- src/test/resources/createTopic.sh
+++ src/test/resources/createTopic.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker exec -ti kafka /opt/kafka/bin/kafka-topics.sh --create --zookeeper 192.168.0.68:2181 --replication-factor --partitions 4 --topic aTopic
+KAFKA_CONTAINER=$(docker ps | grep "wurstmeister/kafka" | awk '{print $1}')
+docker exec -ti ${KAFKA_CONTAINER} /opt/kafka/bin/kafka-topics.sh --create --zookeeper 192.168.0.68:2181 --replication-factor 0 --partitions 4 --topic aTopic
 
